73216ac80df91f94444111d1afa4e641ad526255
Chamud <chamudgeesara@gmail.com>
2023-11-03 19:08:08 +0900

    Attempt input field effects

--- lib/Components/input_field.dart
+++ lib/Components/input_field.dart
@@ -14,7 +14,7 @@ class InputField extends StatefulWidget {
     required this.label,
     required this.controller,
     required this.prefixicon,
-    this.boxWidth = 200,
+    this.boxWidth = 250,
     this.err = '',
     this.ispasswordField = false,
   }) : super(key: key);
@@ -25,11 +25,40 @@ class InputField extends StatefulWidget {
 
 class InputFieldState extends State<InputField> {
   late bool obscureText;
+  final FocusNode containerFocusNode = FocusNode();
+  late List<BoxShadow> containerShadowDecoration = [];
 
   @override
   void initState() {
     super.initState();
     obscureText = widget.ispasswordField;
+
+    containerFocusNode.addListener(() {
+      if (containerFocusNode.hasFocus) {
+        setState(() {
+          containerShadowDecoration.add(
+            const BoxShadow(
+              color: ThemeColours.shadowDark,
+              spreadRadius: -5,
+              offset: Offset(-2.5, -2.5),
+              blurRadius: 5,
+            ),
+          );
+          containerShadowDecoration.add(
+            const BoxShadow(
+              color: ThemeColours.shadowLight,
+              spreadRadius: -5,
+              offset: Offset(2.5, 2.5),
+              blurRadius: 5,
+            ),
+          );
+        });
+      } else {
+        setState(() {
+          containerShadowDecoration = [];
+        });
+      }
+    });
   }
 
   @override
@@ -39,24 +68,85 @@ class InputFieldState extends State<InputField> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TextFormField(
-            controller: widget.controller,
-            obscureText: obscureText,
-            decoration: InputDecoration(
-              hintText: widget.label,
-              prefixIcon: Icon(widget.prefixicon),
-              suffixIcon: widget.ispasswordField
-                  ? IconButton(
-                      icon: Icon(
-                        obscureText ? Icons.visibility : Icons.visibility_off,
+          Container(
+            decoration: BoxDecoration(
+              color: ThemeColours.bgBlueWhite,
+              borderRadius: BorderRadius.circular(NumericConsts.borderRad),
+              border: Border.all(
+                width: 0,
+                color: Colors.transparent,
+              ),
+              boxShadow: const [
+                BoxShadow(
+                  color: ThemeColours.shadowLight,
+                  offset: Offset(-5, -5),
+                  blurRadius: NumericConsts.blurRad,
+                ),
+                BoxShadow(
+                  color: ThemeColours.shadowDark,
+                  offset: Offset(5, 5),
+                  blurRadius: NumericConsts.blurRad,
+                ),
+              ],
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(NumericConsts.blurRad),
+              decoration: BoxDecoration(
+                boxShadow: containerShadowDecoration,
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(NumericConsts.borderRad),
+                border: Border.all(
+                  width: 0,
+                  color: Colors.transparent,
+                ),
+              ),
+              child: Focus(
+                focusNode: containerFocusNode,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: NumericConsts.paddingX,
+                    vertical: 0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: ThemeColours.bgBlueWhite,
+                    borderRadius:
+                        BorderRadius.circular(NumericConsts.borderRad),
+                    border: Border.all(
+                      width: 0,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                  child: TextFormField(
+                    controller: widget.controller,
+                    obscureText: obscureText,
+                    style: const TextStyle(color: ThemeColours.txtBlack),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: widget.label,
+                      hintStyle: const TextStyle(color: ThemeColours.txtGrey),
+                      prefixIcon: Icon(
+                        widget.prefixicon,
+                        color: ThemeColours.iconBlack,
                       ),
-                      onPressed: () {
-                        setState(() {
-                          obscureText = !obscureText;
-                        });
-                      },
-                    )
-                  : null,
+                      suffixIcon: widget.ispasswordField
+                          ? IconButton(
+                              icon: Icon(
+                                obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: ThemeColours.iconBlack,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  obscureText = !obscureText;
+                                });
+                              },
+                            )
+                          : null,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
           Text(
--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -6,13 +6,21 @@ abstract class ThemeColours {
   static const Color bgWhite = Color(0xFFFFFFFF);
 
   // Text colours
-  static const Color txtBlack = Color(0x00000000);
+  static const Color txtBlack = Color(0xFF000000);
   static const Color txtGrey = Color(0x77777777);
   static const Color txtRed = Colors.red;
 
   // Icon Colours
-  static const Color iconBlack = Color(0x00000000);
+  static const Color iconBlack = Color(0xFF000000);
 
   // Shadow Colours
-  static Color shdwGrey = Colors.grey.withOpacity(0.5);
+  static const Color shadowDark = Color(0x88555555);
+  static const Color shadowLight = Color(0x88FFFFFF);
+}
+
+abstract class NumericConsts {
+  static const double borderRad = 50;
+  static const double blurRad = 3;
+  static const double paddingX = 12;
+  static const double paddingY = 5;
 }
